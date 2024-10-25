82f1fd582fc5ea93ef01b0e5a0613dfbc09608a1
tester-fees <tester@fees.world>
2024-10-25 22:47:40 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import AddBoard from "./components/AddBoard.tsx";
 import { StoreBoards } from "./store/StoreBoards.tsx";
 import ListBoards from "./components/ListBoards.tsx";
@@ -17,23 +17,48 @@ function App() {
   const titleRef = useRef<HTMLInputElement>(null);
   const descriptionRef = useRef<HTMLTextAreaElement>(null);
 
+  useEffect(() => {
+    const storedBoards = JSON.parse(localStorage.getItem("boards") as string);
+    boards.map((board) => {
+      if (storedBoards.indexOf(board.name) === -1) {
+        localStorage.setItem("boards", JSON.stringify(boards));
+      }
+    })
+  }, [boards]);
+
   const handleAddBoard = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     const newBoard = {
       id: Math.random().toString(16),
       name: titleRef.current!.value,
       description: descriptionRef.current!.value,
       createdAt: new Date().toJSON().slice(0, 10),
     }
-    setBoards([...boards, newBoard]);
+
+    setBoards((prevState: Boards[]): Boards[] => {
+      if (newBoard.name === '') {
+        return prevState;
+      }
+      const board: Boards | undefined = boards.find((board: Boards): boolean => board.name === newBoard.name);
+      if (board !== undefined) {
+        return [...prevState];
+      } else {
+        return [...prevState, newBoard];
+      }
+    })
   }
 
-
+  //console.log(boards);
 
   return (
     <main>
       <StoreBoards.Provider value={boards}>
+        {
+          boards.map((board) => (
+            <h4>{board.name}</h4>
+          ))
+
+        }
         <ListBoards/>
         <AddBoard
           handleAddBoard={handleAddBoard}
